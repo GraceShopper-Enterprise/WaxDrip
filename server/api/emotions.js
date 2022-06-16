740f72b6a37216ba34b707e08ca83032f2015a73
Andre Barber <andre.barber0505@gmail.com>
2022-06-16 12:14:19 -0500

    AllEmotionsView

--- server/api/emotions.js
+++ server/api/emotions.js
@@ -6,9 +6,19 @@ module.exports = router;
 
 router.get("/", async (req, res, next) => {
   try {
-    const emotions = await Emotion.findAll();
+    const emotions = await Emotion.findAll({ order: [["id", "ASC"]] });
     res.json(emotions);
   } catch (err) {
     next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const emotionDelete = await Emotion.findByPk(req.params.id);
+    await emotionDelete.destroy();
+    res.send(emotionDelete);
+  } catch (error) {
+    next(error);
   }
 });
